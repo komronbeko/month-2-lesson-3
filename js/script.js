73cf74f9310ccd72a1e5faf59a2294dedc549b87
komronbek <kolimov0825@gmail.com>
2022-11-09 02:31:44 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let body = document.querySelector("body");
 let modeBtn = document.querySelector(".nav__div-right__btn-change");
 
-body.addEventListener("click", function(){
+modeBtn.addEventListener("click", function(){
     body.classList.toggle("body-dark")
 })
